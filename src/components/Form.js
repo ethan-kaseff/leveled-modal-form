cc2e38c6c4eb49eacf93c64cb13a7ded79000b84
Ethan Kaseff <ethan@kaseff.com>
2021-10-22 02:00:32 +0100

    checkmark functioning

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Accordion from 'react-bootstrap/Accordion'
+
+import './Form.css'
 
 function Form() {
+  const [one, setOne] = useState(false)
+  const [two, setTwo] = useState(false)
+  const [three, setThree] = useState(false)
+  const [active, setActive] = useState('1')
+
+  useEffect(() => {
+
+  }, [active])
 
   return (
     <>
       <p>Here is the modal</p>
+      <Accordion defaultActiveKey={active}>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            {!one && (
+              <div class='checks'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                </svg>
+              </div>
+            )}
+            {one && (
+              <div class='checks green'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+              </div>
+            )}
+            Initialize Your Wallet
+          </Accordion.Header>
+          <Accordion.Body>
+            <div>
+              To get setup to purchase virtual land with Parcels, you must initialize
+              your wallet, which requires a one time gas fee.
+            </div>
+            <div class='button-div'>
+              <button 
+              onClick={() => {
+                setOne(true);
+                setActive('1')
+              }}
+              class='btn btn-primary button'
+              >
+                Initialize
+              </button>
+            </div>
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>
+            Approve Token
+          </Accordion.Header>
+          <Accordion.Body>
+            Please approve the current token.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            Confirm Listing
+          </Accordion.Header>
+          <Accordion.Body>
+            Accept the signature request in your wallet and wait for your listing 
+            to process. 
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { ModalProvider } from './context/Modal'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <ModalProvider>
