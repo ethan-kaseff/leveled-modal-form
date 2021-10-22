4375d4f4cd2928d7c44deff1d6281e9c7c61e06f
Ethan Kaseff <ethan@kaseff.com>
2021-10-22 02:18:39 +0100

    styling, all three done

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,11 +7,11 @@ function Form() {
   const [one, setOne] = useState(false)
   const [two, setTwo] = useState(false)
   const [three, setThree] = useState(false)
-  const [active, setActive] = useState('1')
+  const [active, setActive] = useState('0')
 
   useEffect(() => {
 
-  }, [active])
+  }, [])
 
   return (
     <>
@@ -20,16 +20,16 @@ function Form() {
         <Accordion.Item eventKey="0">
           <Accordion.Header>
             {!one && (
-              <div class='checks'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+              <div className='checks'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                   <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
                 </svg>
               </div>
             )}
             {one && (
-              <div class='checks green'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+              <div className='checks green'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                 </svg>
               </div>
@@ -37,17 +37,17 @@ function Form() {
             Initialize Your Wallet
           </Accordion.Header>
           <Accordion.Body>
-            <div>
+            <div className='description'>
               To get setup to purchase virtual land with Parcels, you must initialize
               your wallet, which requires a one time gas fee.
             </div>
-            <div class='button-div'>
+            <div className='button-div'>
               <button 
               onClick={() => {
                 setOne(true);
                 setActive('1')
               }}
-              class='btn btn-primary button'
+              className='btn btn-primary'
               >
                 Initialize
               </button>
@@ -56,19 +56,73 @@ function Form() {
         </Accordion.Item>
         <Accordion.Item eventKey="1">
           <Accordion.Header>
+            {!two && (
+              <div className='checks'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                </svg>
+              </div>
+            )}
+            {two && (
+              <div className='checks green'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+              </div>
+            )}
             Approve Token
           </Accordion.Header>
           <Accordion.Body>
-            Please approve the current token.
+            <div className='description'>
+              Please approve the current token.
+            </div>
+            <div className='button-div'>
+              <button
+                onClick={() => {
+                  setTwo(true);
+                }}
+                className='btn btn-primary'
+              >
+                Approve
+              </button>
+            </div>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
           <Accordion.Header>
+            {!three && (
+              <div className='checks'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                </svg>
+              </div>
+            )}
+            {three && (
+              <div className='checks green'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+              </div>
+            )}
             Confirm Listing
           </Accordion.Header>
           <Accordion.Body>
-            Accept the signature request in your wallet and wait for your listing 
-            to process. 
+            <div className='description'>
+              Accept the signature request in your wallet and wait for your listing
+              to process.
+            </div>
+            <div className='button-div'>
+              <button
+                onClick={() => {
+                  setThree(true);
+                }}
+                className='btn btn-primary'
+              >
+                Confirm
+              </button>
+            </div>
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
